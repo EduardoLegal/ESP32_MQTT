2a1481d59ea2e1a8d24a2392cc54f9c3358e70e4
EDUARDO RAMOS PEREIRA <eduardo_pereira24@estudante.sesisenai.org.br>
2025-10-01 16:15:46 -0300

    UPD conexão com wifi

--- sketch_oct1a/sketch_oct1a.ino
+++ sketch_oct1a/sketch_oct1a.ino
@@ -1,11 +1,21 @@
 #include <WiFi.h>
+const String SSID = "iPhone";
+const String PSWD = "iot_sul_123"; 
 
 void scanLocalNetworks();
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Iniciando o Scan de redes Wi-Fi");
   scanLocalNetworks();
+  WiFi.begin(SSID,PSWD);
+  Serial.println("Iniciando conexão com rede Wi-Fi");
+  Serial.print("Conectando");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado!");
+
 }
 
 void loop() {
@@ -13,6 +23,7 @@ void loop() {
 }
 
 void scanLocalNetworks(){
+  Serial.println("Iniciando o Scan de redes Wi-Fi");
   int number = WiFi.scanNetworks();
     delay(500);
     if(number == -1){
