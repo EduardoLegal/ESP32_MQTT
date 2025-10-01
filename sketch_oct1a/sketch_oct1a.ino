eee575fb1c30b6dfb3f059947dd8da0819c71064
EDUARDO RAMOS PEREIRA <eduardo_pereira24@estudante.sesisenai.org.br>
2025-10-01 15:36:31 -0300

    UPD com função para aparecer uma só vez

--- sketch_oct1a/sketch_oct1a.ino
+++ sketch_oct1a/sketch_oct1a.ino
@@ -1,21 +1,28 @@
 #include <WiFi.h>
 
+void scanLocalNetworks();
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Iniciando o Scan de redes Wi-Fi");
+  scanLocalNetworks();
 }
 
 void loop() {
-  int number = WiFi.scanNetworks();
-  delay(500);
-  if(number == -1){
-    Serial.prinln("ERRO!, fui mogado");
-  }
-  else{
-    for(int net = 0; net < net; net++){
-      Serial.printf("%d - %s | %d db\n",net,WiFi.SSID(net),WiFi.RSSI(net));
-    }
-    Serial.prinf("Número de redes encontradas: %d\n", number);
+  
+}
 
-  }
+void scanLocalNetworks(){
+  int number = WiFi.scanNetworks();
+    delay(500);
+    if(number == -1){
+      Serial.prinln("ERRO!, problema em encontrar alguma rede");
+    }
+    else{
+      for(int net = 0; net < net; net++){
+        Serial.printf("%d - %s | %d db\n",net,WiFi.SSID(net),WiFi.RSSI(net));
+      }
+      Serial.prinf("Número de redes encontradas: %d\n", number);
+
+    }
 }
