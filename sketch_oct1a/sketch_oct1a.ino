f6f43b012cae67536a34f73b4856d2da98185b27
EDUARDO RAMOS PEREIRA <eduardo_pereira24@estudante.sesisenai.org.br>
2025-10-01 15:25:19 -0300

    UPD Scan de net

--- sketch_oct1a/sketch_oct1a.ino
+++ sketch_oct1a/sketch_oct1a.ino
@@ -1,11 +1,21 @@
-#include <Wifi.h>
+#include <WiFi.h>
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  Serial.println("Iniciando o Scan de redes Wi-Fi");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int number = WiFi.scanNetworks();
+  delay(500);
+  if(number == -1){
+    Serial.prinln("ERRO!, fui mogado");
+  }
+  else{
+    for(int net = 0; net < net; net++){
+      Serial.printf("%d - %s | %d db\n",net,WiFi.SSID(net),WiFi.RSSI(net));
+    }
+    Serial.prinf("Número de redes encontradas: %d\n", number);
 
+  }
 }
